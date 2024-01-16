0717c70226ae2d81e465325216169d2069898d35
thecraftianman <64441307+thecraftianman@users.noreply.github.com>
2024-01-16 13:07:22 -0500

    Fix client startup error

--- lua/vrmodunoffcial/vrmod_lvsbeta.lua
+++ lua/vrmodunoffcial/vrmod_lvsbeta.lua
@@ -1,15 +1,15 @@
+if not SERVER then return end
 if not LVS then return end
-
 
 util.AddNetworkString("lvs_setinput")
 -- ネットワークメッセージを受け取る
 net.Receive(
     "lvs_setinput",
-    function(len, ply)
+    function(_, ply)
         local inputName = net.ReadString()
         local inputValue = net.ReadBool()
         if IsValid(ply) then
-            local vehicle = ply:lvsGetVehicle()
+            -- local vehicle = ply:lvsGetVehicle()
 
             ply:lvsSetInput(inputName, inputValue)
             if inputName == "EXIT" and inputValue == true then
